dfee4435e5782ad47c17015e8d8636d01436e9c7
Han Yejong <sdjongil@naver.com>
2025-08-08 14:50:23 +0900

    feat: New branch init

--- storeService/src/main/java/com/example/storeservice/StoreController.java
+++ storeService/src/main/java/com/example/storeservice/StoreController.java
@@ -1,13 +1,8 @@
 package com.example.storeservice;
 
 
-import com.example.commonlibrary.ApiResponse;
-import com.example.storeservice.dto.StoreDto;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/store")
 @RequiredArgsConstructor
 public class StoreController {
-    private final StoreService storeService;
+    private final StoreService service;
 
 
-    @GetMapping("/{storeId}")
-    public ResponseEntity<ApiResponse<?>> getStore(
-            @PathVariable String storeId,
-            HttpServletRequest request
-    ){
+    @GetMapping("/")
+    public String getStoreInformation(String StoreId){
 
 
 
