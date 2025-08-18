5130d4328045803fd8792d8d3fe835b715059885
hystrix96 <leo8071004@naver.com>
2025-08-18 13:54:10 +0900

    build: Build authorization microservice #9

---
+++ msa-auth-service/src/main/java/com/example/msaauthservice/MsaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msaauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaAuthServiceApplication.class, args);
+    }
+
+}
