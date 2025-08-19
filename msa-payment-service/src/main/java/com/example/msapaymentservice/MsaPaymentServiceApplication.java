63ab5143461027a8be51eb2583a7d38db42b23c7
DK.LEE <jjk1491@gmail.com>
2025-08-19 16:57:08 +0900

    build: Init payment module #20

---
+++ msa-payment-service/src/main/java/com/example/msapaymentservice/MsaPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msapaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaPaymentServiceApplication.class, args);
+	}
+
+}
