ad00dccf16d7c6d82b68bd959e256271d74f7e54
kimsumin <rlatnaln3255@naver.com>
2025-08-26 18:45:21 +0900

    feat: Add auth yml all  #26

---
+++ msa-order-service/src/main/java/com/example/msaorderservice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.msaorderservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/orders/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
